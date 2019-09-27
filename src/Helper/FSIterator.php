6aada258b016b56a4d1a3235ad5e9bb556f38296
Marc MOREAU <moreau.marc.web@gmail.com>
2019-09-27 16:20:20 +0200

    remove detach on delete

---
+++ src/Helper/FSIterator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: utilisateur
+ * Date: 27/09/19
+ * Time: 14:47
+ */
+
+namespace MockingMagician\Organic\Helper;
+
+
+class FSIterator
+{
+
+}
---
+++ src/Helper/FSIteratorOnlyDir.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MockingMagician\Organic\Helper;
+
+
+use MockingMagician\Organic\Exception\DirectoryPathException;
+use Traversable;
+
+class FSIteratorOnlyFiles implements \IteratorAggregate
+{
+    private $path;
+
+    public function __construct(string $path)
+    {
+        if (!is_dir($path)) {
+            throw new DirectoryPathException($path);
+        }
+        $this->path = realpath($path);
+    }
+
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     * @since 5.0.0
+     */
+    public function getIterator()
+    {
+        $scanDir = array_filter($this->scanDir(), function ($value) {
+            if (is_file($value)) {
+                return true;
+            }
+
+            return false;
+        });
+
+        return new \ArrayIterator($this->path.DIRECTORY_SEPARATOR.$scanDir);
+    }
+
+    protected function scanDir()
+    {
+        return array_diff(scandir($this->path), array('..', '.'));
+    }
+}
