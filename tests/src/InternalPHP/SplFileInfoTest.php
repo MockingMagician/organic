62a687d3f34970b4188ecbdf0623a9f39758586d
Marc MOREAU <moreau.marc.web@gmail.com>
2019-10-03 12:35:41 +0200

    snapshot

---
+++ tests/src/InternalPHP/SplFileInfoTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: utilisateur
+ * Date: 01/10/19
+ * Time: 22:48
+ */
+
+namespace MockingMagician\Organic\Tests\InternalPHP;
+
+use SplFileInfo;
+use PHPUnit\Framework\TestCase;
+
+class SplFileInfoTest extends TestCase
+{
+
+}
