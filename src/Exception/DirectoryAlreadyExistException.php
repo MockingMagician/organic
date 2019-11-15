dc77c59b8cbb2e383d47b3bb2b9f463d772a033d
Marc MOREAU <moreau.marc.web@gmail.com>
2019-11-15 22:27:17 +0100

    n/a

--- src/Exception/DirectoryAlreadyExistException.php
+++ src/Exception/DirectoryAlreadyExistException.php
@@ -3,7 +3,6 @@
 /**
  * @author Marc MOREAU <moreau.marc.web@gmail.com>
  * @license https://github.com/MockingMagician/organic/blob/master/LICENSE.md CC-BY-SA-4.0
- *
  * @link https://github.com/MockingMagician/organic/blob/master/README.md
  */
 
